93e54006f9c5971651673de644853016e9bed182
scriptSQD <73490413+scriptSQD@users.noreply.github.com>
2022-03-12 23:42:55 +0300

    [I] - Implement new Content-Types. [I] - Implement security logic (may be poor now), so that user can only access his own orders, not other's ones. [M] - In addition to the above, users/me request now returns Orders too.

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET", "e3b8eeb71807743c13312b48a6b67a0b"),
   },
 });
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-'use strict';
+"use strict";
+
+const { sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
   /**
@@ -7,7 +9,45 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+    strapi.service("plugin::users-permissions.user").fetchAuthenticatedUser =
+      function (id) {
+        return strapi.query("plugin::users-permissions.user").findOne({
+          where: { id },
+          populate: ["role", "orders"],
+        });
+      };
+    strapi.controller("api::order.order").find = async function (ctx) {
+      let entities;
+
+      if (ctx.query._q) {
+        entities = await strapi.service("api::order.order").search(ctx.query);
+      } else {
+        entities = await strapi.service("api::order.order").find({
+          id: ctx.query,
+          populate: ["user"],
+        });
+      }
+
+      let orderIds = [],
+        orders = [];
+
+      entities.results.forEach((e) => {
+        if (ctx.state.user.id === e.user.id) orderIds.push(e.id);
+      });
+
+      for (let id of orderIds) {
+        let entry;
+        entry = await strapi.service("api::order.order").findOne(id, {
+          populate: ["modifications", "specifications", "specifications.pcb"],
+          // populate field to change when new relations are added
+        });
+        orders.push(entry);
+      }
+
+      return orders;
+    };
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
